7117e96c9ef92a5c6321f6d3e59a3878e333240a
Jodie Alaine Parker <me@jodiealaine.com>
2016-08-26 07:00:05 +0100

    Add web tooling and automation

---
+++ app/scripts/events.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$.getJSON('../data/events.json', function(events) {
+		$.each(events, (function(i, event) {
+			var eventPanel = '<div class="col s12 m4">' +
+			'<div class="card-panel">' +
+			'<h4 class="card-title name">' + event.name + '</h4>' +
+			'<br>' +
+			'<p class="type">Type:' + event.type + '</p>' +
+			'<p class="host">Host:' + event.host + '</p>' +
+			'<p class="start-date">Start: ' + event.start_date + '</p>' +
+			'<p class="end-date">End: ' + event.end_date + '</p>' +
+			'<p class="guests">Guests:</p>' +
+			'<ul class="guest-list" id=' + event.id + '>' +
+			'</ul>' +
+			'<p class="location">Location: ' + event.location + '</p>' +
+			'<p class="message">Message: ' + event.message + '</p>' +
+			'</div>' +
+			'</div>'; 
+
+			$('#events').append(eventPanel);
+
+			var listItemID = '#' + event.id;
+			
+			$.each(event.guest_list, (function(i, guest) {
+				var guestListItem = '<li class="guest">' + guest + '</li>'
+				$(listItemID).append(guestListItem);
+			}))
+		}))
+	});
+});
